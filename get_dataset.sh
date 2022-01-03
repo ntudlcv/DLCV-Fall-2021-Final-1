8a8afea645cca21746413591640c3f8da35125b7
ntudlcv <72860306+ntudlcv@users.noreply.github.com>
2022-01-03 17:14:43 +0800

    Update get_dataset.sh

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,5 +1,5 @@
 # Download dataset from Dropbox
-wget "https://www.dropbox.com/s/yp39kgda1kqzdvu/skull.zip?dl=0" -O skull.zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1dKWCyios4wyUWmyNP72BFE5dVp2uYXIN' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1dKWCyios4wyUWmyNP72BFE5dVp2uYXIN" -O skull.zip && rm -rf /tmp/cookies.txt
 
 # Unzip the downloaded zip file
 unzip ./skull.zip
